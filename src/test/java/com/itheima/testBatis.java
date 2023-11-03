e184e48c5a7025d9c65116f1ca57eec0125236e8
ROOT <amourzeta@163.com>
2023-11-03 20:45:03 +0800

    LambdaQuery

--- src/test/java/com/itheima/testBatis.java
+++ src/test/java/com/itheima/testBatis.java
@@ -1,11 +1,14 @@
 package com.itheima;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.mapper.UserMapper;
 import com.itheima.pojo.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -19,5 +22,14 @@ public class testBatis {
         User user = userMapper.selectById(1);
         log.info("hello:{}",user.toString());
         System.out.println(user.getName());
+    }
+
+    @Test
+    public void fun02(){
+        LambdaQueryWrapper<User> lambda = new LambdaQueryWrapper<>();
+        lambda.gt(User::getAge,20)
+                .likeRight(User::getName,"张");
+        List<User> users = userMapper.selectList(lambda);
+
     }
 }
